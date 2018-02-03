f7630992d5af3842ae908fcb7dc0ef1d09819d62
andreashosbach <andreas.hosbach@gmail.com>
2018-02-03 16:57:50 +0100

    Added configuration options

--- display.py
+++ display.py
@@ -1,36 +1,47 @@
 #!/usr/bin/python
 
-import sys
 from threading import Timer
 from config import get_config
 from config import read_config
 import cberry
-import thingspeak
 from thingspeak import get_thingspeak_field_feed
 import json
 
-# =============================================================================
-# Measure and write every x seconds
-# =============================================================================
-# Resets the timer
 def display():
+# Resets the timer
     Timer(float(get_config("timer_wait")), display).start()
     
-    channel_id = 398939
-    field_id = 1
-    read_api_key = "RX6KEENEBPKDVN9L"
-    number_of_results = 1
+    for field in get_config("display"):
+        x = field["x"]
+        y = field["y"]
+        font = field["font"]
+        text = field["label"]
+        
+        data_name = field["value"]
 
-    data = get_thingspeak_field_feed(channel_id, field_id, read_api_key, number_of_results)
-    dict = json.loads(data)
-    temperature = dict["feeds"][0]["field1"]; 
-    cberry.writeText(10, 100, 5, str(temperature), cberry.BLACK, cberry.WHITE)
-      
+        data = get_config("data")
+        field_id = data[data_name]["field_id"] 
+        channel_name = data[data_name]["channel"]
+        
+        channels = get_config("channels")
+        channel = channels[channel_name]
+        channel_id = channel["channel_id"]
+        read_api_key = channel["read_api_key"]
+        
+        number_of_results = 1
+
+        raw_value = get_thingspeak_field_feed(channel_id, field_id, read_api_key, number_of_results)
+        dict_value = json.loads(raw_value)
+        value = dict_value["feeds"][0]["field" + str(field_id)]; 
+        
+        if(data[type] == "FLOAT"):
+            value = float(value)
+            
+        text = text % (value)
+        cberry.writeText(x, y, font, text, cberry.BLACK, cberry.WHITE)
     
-# =============================================================================
-# Main ---
-# =============================================================================
 def main():
+# Main 
     print("Reading configuration")
     print(read_config())
     cberry.initScreen()
